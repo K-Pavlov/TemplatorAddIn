8280d8a12f31cc52b9f55db08cf4f6a4396d35be
Kaloyan <kaloian.pavlov@abv.bg>
2014-07-14 16:41:39 +0300

    Big initial commit

---
+++ Templator/FormForPlugin/TemplateForm.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace AddionalPluginUtilities
+{
+    partial class TemplateForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxModule = new System.Windows.Forms.TextBox();
+            this.textBoxMethod = new System.Windows.Forms.TextBox();
+            this.textBoxLanguage = new System.Windows.Forms.TextBox();
+            this.textBoxText = new System.Windows.Forms.TextBox();
+            this.textLabelName = new System.Windows.Forms.Label();
+            this.textLabelEnglish = new System.Windows.Forms.Label();
+            this.textLabelGerman = new System.Windows.Forms.Label();
+            this.textLabelID = new System.Windows.Forms.Label();
+            this.textBoxTemplate = new System.Windows.Forms.TextBox();
+            this.textLabelTemplate = new System.Windows.Forms.Label();
+            this.writeButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxModule
+            // 
+            this.textBoxModule.BackColor = System.Drawing.Color.Firebrick;
+            this.textBoxModule.Location = new System.Drawing.Point(78, 63);
+            this.textBoxModule.Name = "textBoxModule";
+            this.textBoxModule.Size = new System.Drawing.Size(100, 20);
+            this.textBoxModule.TabIndex = 0;
+            // 
+            // textBoxMethod
+            // 
+            this.textBoxMethod.BackColor = System.Drawing.Color.Firebrick;
+            this.textBoxMethod.Location = new System.Drawing.Point(77, 89);
+            this.textBoxMethod.Name = "textBoxMethod";
+            this.textBoxMethod.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMethod.TabIndex = 1;
+            // 
+            // textBoxLanguage
+            // 
+            this.textBoxLanguage.BackColor = System.Drawing.Color.Firebrick;
+            this.textBoxLanguage.Location = new System.Drawing.Point(77, 115);
+            this.textBoxLanguage.Name = "textBoxLanguage";
+            this.textBoxLanguage.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLanguage.TabIndex = 2;
+            // 
+            // textBoxText
+            // 
+            this.textBoxText.BackColor = System.Drawing.Color.Firebrick;
+            this.textBoxText.Location = new System.Drawing.Point(77, 141);
+            this.textBoxText.Name = "textBoxText";
+            this.textBoxText.Size = new System.Drawing.Size(100, 20);
+            this.textBoxText.TabIndex = 3;
+            // 
+            // textLabelName
+            // 
+            this.textLabelName.AutoSize = true;
+            this.textLabelName.Location = new System.Drawing.Point(29, 66);
+            this.textLabelName.Name = "textLabelName";
+            this.textLabelName.Size = new System.Drawing.Size(42, 13);
+            this.textLabelName.TabIndex = 4;
+            this.textLabelName.Text = "Module";
+            // 
+            // textLabelEnglish
+            // 
+            this.textLabelEnglish.AutoSize = true;
+            this.textLabelEnglish.Location = new System.Drawing.Point(23, 92);
+            this.textLabelEnglish.Name = "textLabelEnglish";
+            this.textLabelEnglish.Size = new System.Drawing.Size(48, 13);
+            this.textLabelEnglish.TabIndex = 6;
+            this.textLabelEnglish.Text = "Function";
+            // 
+            // textLabelGerman
+            // 
+            this.textLabelGerman.AutoSize = true;
+            this.textLabelGerman.Location = new System.Drawing.Point(16, 118);
+            this.textLabelGerman.Name = "textLabelGerman";
+            this.textLabelGerman.Size = new System.Drawing.Size(55, 13);
+            this.textLabelGerman.TabIndex = 8;
+            this.textLabelGerman.Text = "Language";
+            // 
+            // textLabelID
+            // 
+            this.textLabelID.AutoSize = true;
+            this.textLabelID.Location = new System.Drawing.Point(29, 144);
+            this.textLabelID.Name = "textLabelID";
+            this.textLabelID.Size = new System.Drawing.Size(28, 13);
+            this.textLabelID.TabIndex = 10;
+            this.textLabelID.Text = "Text";
+            // 
+            // textBoxTemplate
+            // 
+            this.textBoxTemplate.BackColor = System.Drawing.Color.Firebrick;
+            this.textBoxTemplate.Location = new System.Drawing.Point(39, 12);
+            this.textBoxTemplate.Name = "textBoxTemplate";
+            this.textBoxTemplate.Size = new System.Drawing.Size(138, 20);
+            this.textBoxTemplate.TabIndex = 11;
+            // 
+            // textLabelTemplate
+            // 
+            this.textLabelTemplate.AutoSize = true;
+            this.textLabelTemplate.Location = new System.Drawing.Point(84, 35);
+            this.textLabelTemplate.Name = "textLabelTemplate";
+            this.textLabelTemplate.Size = new System.Drawing.Size(51, 13);
+            this.textLabelTemplate.TabIndex = 12;
+            this.textLabelTemplate.Text = "Template";
+            // 
+            // writeButton
+            // 
+            this.writeButton.Location = new System.Drawing.Point(30, 193);
+            this.writeButton.Name = "writeButton";
+            this.writeButton.Size = new System.Drawing.Size(75, 23);
+            this.writeButton.TabIndex = 13;
+            this.writeButton.Text = "Write";
+            this.writeButton.UseVisualStyleBackColor = true;
+            this.writeButton.Click += new System.EventHandler(this.writeButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(111, 193);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 14;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.BackColor = System.Drawing.Color.Firebrick;
+            this.textBoxId.Location = new System.Drawing.Point(77, 167);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(100, 20);
+            this.textBoxId.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "ID";
+            // 
+            // TemplateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(220, 217);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.writeButton);
+            this.Controls.Add(this.textLabelTemplate);
+            this.Controls.Add(this.textBoxTemplate);
+            this.Controls.Add(this.textLabelID);
+            this.Controls.Add(this.textLabelGerman);
+            this.Controls.Add(this.textLabelEnglish);
+            this.Controls.Add(this.textLabelName);
+            this.Controls.Add(this.textBoxText);
+            this.Controls.Add(this.textBoxLanguage);
+            this.Controls.Add(this.textBoxMethod);
+            this.Controls.Add(this.textBoxModule);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TemplateForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TemplateForm_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxModule;
+        private System.Windows.Forms.TextBox textBoxMethod;
+        private System.Windows.Forms.TextBox textBoxLanguage;
+        private System.Windows.Forms.TextBox textBoxText;
+        private System.Windows.Forms.Label textLabelName;
+        private System.Windows.Forms.Label textLabelEnglish;
+        private System.Windows.Forms.Label textLabelGerman;
+        private System.Windows.Forms.Label textLabelID;
+        private System.Windows.Forms.TextBox textBoxTemplate;
+        private System.Windows.Forms.Label textLabelTemplate;
+        private System.Windows.Forms.Button writeButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Templator/FormForPlugin/TemplateForm.cs
@@ -0,0 +1,283 @@
+﻿namespace AddionalPluginUtilities
+{
+    using System;
+    using System.Data;
+    using System.Data.SqlClient;
+    using System.Text.RegularExpressions;
+    using System.Windows.Forms;
+    using Extensibility;
+    using EnvDTE;
+    using EnvDTE80;
+    using Microsoft.VisualStudio.CommandBars;
+    using System.Runtime.InteropServices;
+
+    public partial class TemplateForm : Form
+    {
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int HT_CAPTION = 0x2;
+        private const int WM_NCLBUTTONDBLCLK = 0x00A3;
+
+        private SqlConnection connection;
+        private DTE2 applicationObject;
+
+        public DTE2 ApplicationObject
+        {
+            get
+            {
+                return this.applicationObject;
+            }
+
+            private set
+            {
+                this.applicationObject = value;
+            }
+        }
+
+
+        public TemplateForm(DTE2 applicationObject)
+        {
+            InitializeComponent();
+            this.connection = new SqlConnection("Server=localhost;" +
+            "Integrated security=SSPI;" +
+            "database=master;");
+
+            this.ApplicationObject = applicationObject;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_NCLBUTTONDBLCLK)
+            {
+                m.Result = IntPtr.Zero;
+                return;
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private void writeButton_Click(object sender, EventArgs e)
+        {
+            string template = this.textBoxTemplate.Text,
+                module = this.textBoxModule.Text,
+                method = this.textBoxMethod.Text,
+                language = this.textBoxLanguage.Text,
+                text = this.textBoxText.Text,
+                id = this.textBoxId.Text,
+                templatedText;
+
+            var document = this.applicationObject.ActiveDocument;
+
+            if (document == null)
+            {
+                MessageBox.Show("No document opened");
+                return;
+            }
+
+            if (template == "")
+            {
+                templatedText = "\"" + module + " " + method + " "
+                    + language + " " + text + " " + id + "\"";
+            }
+            else
+            {
+                templatedText = "\"";
+                templatedText += Regex.Replace(template, "{{module}}", module, RegexOptions.IgnoreCase);
+                templatedText = Regex.Replace(templatedText, "{{method}}", method, RegexOptions.IgnoreCase);
+                templatedText = Regex.Replace(templatedText, "{{language}}", language, RegexOptions.IgnoreCase);
+                templatedText = Regex.Replace(templatedText, "{{text}}", text, RegexOptions.IgnoreCase);
+                templatedText = Regex.Replace(templatedText, "{{id}}", id, RegexOptions.IgnoreCase);
+                templatedText += "\"";
+            }
+
+            this.InsertTemplate(document, templatedText);
+
+            //
+            // 
+
+            try
+            {
+                this.connection.Open();
+                SqlCommand writeCommand = PrepareWriteCommand(new Values(module, method, language, text, id));
+                writeCommand.ExecuteNonQuery();
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Addin", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            finally
+            {
+                if (this.connection.State == ConnectionState.Open)
+                {
+                    this.connection.Close();
+                }
+            }
+        }
+
+        private void InsertTemplate(Document document, string toInsert)
+        {
+            var textDocument = document.Object() as TextDocument;
+
+            textDocument.StartPoint.CreateEditPoint();
+            textDocument.Selection.Insert(toInsert);
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private SqlCommand PrepareWriteCommand(Values values)
+        {
+            SqlCommand command = new SqlCommand(null, this.connection);
+            SqlParameter module = new SqlParameter("@module", SqlDbType.VarChar, 50),
+                method = new SqlParameter("@method", SqlDbType.VarChar, 50),
+                language = new SqlParameter("@language", SqlDbType.VarChar, 50),
+                text = new SqlParameter("@text", SqlDbType.Text),
+                id = new SqlParameter("@id", SqlDbType.Int);
+
+            command.CommandText =
+                "INSERT INTO AddinInfo.dbo.addinContent (module, method, language, text, id) "
+                + "VALUES (@module, @method, @language, @text, @id)";
+
+            module.Value = values.Module;
+            method.Value = values.Method;
+            language.Value = values.Language;
+            text.Value = values.Text;
+            id.Value = values.Id;
+
+            command.Parameters.Add(module);
+            command.Parameters.Add(method);
+            command.Parameters.Add(language);
+            command.Parameters.Add(text);
+            command.Parameters.Add(id);
+
+            command.Prepare();
+
+            return command;
+        }
+
+        private void TemplateForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private struct Values
+        {
+            private string module;
+            private string method;
+            private string language;
+            private string text;
+            private int id;
+
+            public string Text
+            {
+                get
+                {
+                    return this.text;
+                }
+
+                private set
+                {
+                    this.text = value;
+                }
+            }
+
+
+            public int Id
+            {
+                get
+                {
+                    return this.id;
+                }
+
+                private set
+                {
+                    this.id = value;
+                }
+            }
+
+
+            public string Language
+            {
+                get
+                {
+                    return this.language;
+                }
+
+                private set
+                {
+                    this.language = value;
+                }
+            }
+
+            public string Method
+            {
+                get
+                {
+                    return this.method;
+                }
+
+                private set
+                {
+                    this.method = value;
+                }
+            }
+
+
+            public string Module
+            {
+                get
+                {
+                    return this.module;
+                }
+
+                private set
+                {
+                    this.module = value;
+                }
+            }
+
+            public Values(string module, string method, string language, string text, string id)
+            {
+                this.module = "";
+                this.method = "";
+                this.language = "";
+                this.text = "";
+                this.id = 0;
+
+                this.Module = module;
+                this.Method = method;
+                this.Language = language;
+                this.text = text;
+                this.Id = validateId(id);
+            }
+
+            private int validateId(string id)
+            {
+                int number;
+                bool result;
+
+                result = int.TryParse(id, out number);
+
+                if (result)
+                {
+                    return number;
+                }
+                else
+                {
+                    throw new ArgumentException("Id must be a number");
+                }
+            }
+        }
+    }
+}
